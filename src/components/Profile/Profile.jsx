909ff7a28a2bdc44dec96d7f4325c4789dea6c5f
i-havr <i.havrylov@gmail.com>
2022-12-16 13:03:06 +0200

    fix profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -27,12 +27,16 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
       </ul>
     </div>
   );
-}
+};
 
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.object.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
